f0d2165b02ed6e6f5b06a7e303f1b7be7e57e285
Mark Lisé <mark@digitalspace.ca>
2024-09-17 10:06:26 -0700

    Update UT's

--- src/app/services/amplify.service.spec.ts
+++ src/app/services/amplify.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AmplifyService } from './amplify.service';
+import { ConfigService } from './config.service';
+import { LogLevel } from './logger.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 describe('AmplifyService', () => {
   let service: AmplifyService;
-
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [HttpClient, HttpHandler, ConfigService]
+    });
     service = TestBed.inject(AmplifyService);
   });
 
