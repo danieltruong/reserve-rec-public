9a229d944252ff7d4e810b16421c97c4a1f2ac2c
Daniel Truong <daniel@digitalspace.ca>
2024-10-18 00:21:25 +0000

    Removing unneeded route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,5 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   { path: '', loadComponent: () => import('./home/home.component').then(mod => mod.HomeComponent) },
   { path: 'login', loadComponent: () => import('./login/login.component').then(mod => mod.LoginComponent) },
-  { path: 'search', loadComponent: () => import('./search/search.component').then(mod => mod.SearchComponent) },
-  { path: 'map', loadComponent: () => import('./big-map/big-map.component').then(mod => mod.BigMapComponent) },
+  { path: 'search', loadComponent: () => import('./search/search.component').then(mod => mod.SearchComponent) }
 ];
